ccd549a032ebd5e6f524040ada00d6cff15ffef9
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-04-03 18:56:30 -0500

    asdas

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginForm from "./Components/LoginForm/LoginForm";
 import RegisterForm from "./Components/RegisterForm/RegisterForm";
+import UserView from "./Pages/UserView/UserView";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Routes>
         <Route exact path="/register" element={<RegisterForm />}></Route>
         <Route exact path="/login" element={<LoginForm />}></Route>
+        <Route exact path="/userView" element={<UserView />}></Route>
         <Route exact path="*" element={<RegisterForm />}></Route>
       </Routes>
     </BrowserRouter>
